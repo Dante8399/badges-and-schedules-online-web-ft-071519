b6bb9e4c73b1b78ba469719c451d75f931270661
Dante Spencer <spencerdantel@gmail.com>
2019-07-20 22:35:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}." 
+ end
+  
+  
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array << "Hello, my name is #{name}."
+  end
+  new_array
+end
